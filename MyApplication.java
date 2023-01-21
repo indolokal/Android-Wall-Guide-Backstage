f738018e7c63e609dffceec73f21019f966864a9
IndoLokal.id <mybbidgroup@gmail.com>
2023-01-22 03:11:39 +0700

    Update MyApplication.java

--- MyApplication.java
+++ MyApplication.java
@@ -1,32 +1,13 @@
-    
-
-private void requestAPI(String remoteUrl) {
-        if (remoteUrl.startsWith("http://") || remoteUrl.startsWith("https://")) {
-            if (remoteUrl.contains("https://drive.google.com")) {
-                String driveUrl = remoteUrl.replace("https://", "").replace("http://", "");
-                List<String> data = Arrays.asList(driveUrl.split("/"));
-                String googleDriveFileId = data.get(3);
-                callbackConfigCall = RestAdapter.createApi().getDriveJsonFileId(googleDriveFileId);
-            } else {
-                callbackConfigCall = RestAdapter.createApi().getJsonUrl(remoteUrl);
-            }
-        } else {
-            callbackConfigCall = RestAdapter.createApi().getDriveJsonFileId(remoteUrl);
+    private void requestConfig() {
+        if (!Config.ACCESS_KEY.contains("XXXXX")) {
+            String data = Tools.decode(Config.ACCESS_KEY);
+            String[] results = data.split("_applicationId_");
+            String remoteUrl = results[0];
+            requestAPI(remoteUrl);
         }
-        callbackConfigCall.enqueue(new Callback<CallbackConfig>() {
-            public void onResponse(@NonNull Call<CallbackConfig> call, @NonNull Response<CallbackConfig> response) {
-                CallbackConfig resp = response.body();
-                if (resp != null) {
-                    notification = resp.notification;
-                    FirebaseMessaging.getInstance().subscribeToTopic(notification.fcm_notification_topic);
-                    OneSignal.setAppId(notification.onesignal_app_id);
-                    Log.d(TAG, "FCM Subscribe topic : " + notification.fcm_notification_topic);
-                    Log.d(TAG, "OneSignal App ID : " + notification.onesignal_app_id);
-                }
-            }
+    }
 
-            public void onFailure(@NonNull Call<CallbackConfig> call, @NonNull Throwable th) {
-                Log.e(TAG, "initialize failed : " + th.getMessage());
-            }
-        });
+    private void requestConfig() {
+        String remoteUrl = Config.ACCESS_KEY;
+        requestAPI(remoteUrl);
     }
